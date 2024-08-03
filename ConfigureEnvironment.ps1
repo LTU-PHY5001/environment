9ecfa7cdfc5ab696ee47d9555c6da82c7d605855
gvrx <62734313+GVRX@users.noreply.github.com>
2024-08-04 02:12:05 +1000

    Fixed script

--- ConfigureEnvironment.ps1
+++ ConfigureEnvironment.ps1
@@ -1,6 +1,6 @@
 
 pip install virtualenv
-python3 -m venv mqit
+python -m venv mqit
 .\mqit\Scripts\Activate.ps1 
 pip install --no-cache -r requirements.txt 
 
@@ -8,16 +8,9 @@ pip install --no-cache -r requirements.txt
 python -m ipykernel install --user --name=mqit_kernel 
 
 # install VS Code
-wget https://code.visualstudio.com/sha/download?build=stable&os=cli-win32-x64
-VSCodeUserSetup-x64-1.86.2.exe /VERYSILENT /MERGETASKS=!runcode
+curl -o VSCode.exe  'https://code.visualstudio.com/sha/download?build=stable&os=cli-win32-x64'
+.\VSCode.exe /VERYSILENT /MERGETASKS=!runcode
 
 # Install Git
-curl https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe
-./Git-2.46.0-64-bit.exe /LOADINF="git.inf".
-
-
-#install VS Code extensions
-code --install-extension quantum.qsharp-lang-vscode	
-code --install-extension ms-toolsai.jupyter
-
-
+curl -o GitInstall.exe https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe
+.\GitInstall.exe /VER
