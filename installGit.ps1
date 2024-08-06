bf3d1c6c4dc351bfcf68da198f892afb7ab9b8d3
gvrx <62734313+GVRX@users.noreply.github.com>
2024-08-06 14:38:33 +1000

    separated script for git install

---
+++ installGit.ps1
@@ -0,0 +1,9 @@
+
+# Install Git
+curl -o GitInstall.exe https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe
+.\GitInstall.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOADINF="git.inf"
+
+# Add git CLI utilities to path
+$PATH = [Environment]::GetEnvironmentVariable("PATH")
+$git_path = "$($env:LOCALAPPDATA)\Programs\Git\bin"
+[Environment]::SetEnvironmentVariable("PATH", "$PATH;$git_path")
