3b161c64b591c250cc55d2222519a040c1d385de
gvrx <62734313+GVRX@users.noreply.github.com>
2024-08-06 23:07:02 +1000

    clean up

--- replaceVDI-tag.sh
+++
@@ -1,6 +0,0 @@
-#Delete the tag on any remote before you push
-git push origin :refs/tags/VDI
-#Replace the tag to reference the most recent commit
-git tag -fa VDI -m "Updated for VDI deployment"
-#Push the tag to the remote origin
-git push origin main --tags
