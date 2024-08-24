129dd73fb337b475959d8fc825848b223c24bba2
gvrx <62734313+GVRX@users.noreply.github.com>
2024-08-24 22:31:42 +1000

    updated install git script

--- installGit.ps1
+++ installGit.ps1
@@ -1,12 +1,28 @@
 
 # Install Git
-curl -o GitInstall.exe https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe
-.\GitInstall.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOADINF="git.inf"
+
+Write-Host "`n Installing GIT..." -ForegroundColor Yellow
+
+$installer = "./GitInstall.exe"
+
+try {
+    curl -o $installer https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe
+    $installer /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOADINF="git.inf"
+    }
+catch {
+    Write-Host  "download failed"
+    }
+
 
 # Add git CLI utilities to path
 $PATH = [Environment]::GetEnvironmentVariable("PATH")
 $git_path = "$($env:LOCALAPPDATA)\Programs\Git\bin"
 [Environment]::SetEnvironmentVariable("PATH", "$PATH;$git_path")
 
-Remove-item GitInstall.exe 
+try {
+    Remove-item $installer
+    }
+catch {
+    Write-Host "Removing $installer failed"
+    }
 
