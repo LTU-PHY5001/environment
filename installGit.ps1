466ebaa24c42308d099fe16dd1c8b86670842181
gvrx <62734313+GVRX@users.noreply.github.com>
2024-11-07 10:45:34 +1100

    fixed path for Git install

--- installGit.ps1
+++ installGit.ps1
@@ -10,12 +10,9 @@ $PATH = [Environment]::GetEnvironmentVariable("PATH")
 $git_path = "$($mqitPath)\Git"
 [Environment]::SetEnvironmentVariable("PATH", "$PATH;$git_path\bin")
 
-try {
-    curl -o "./GitInstall.exe" "https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe"
-    ./GitInstall.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOADINF="git.inf"
-    }
-catch {
-    Write-Host  "download failed"
-    }
+Add-Content -Path "git.inf" -Value "Dir=$($git_path)"
+
+curl -o "./GitInstall.exe" "https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe"
+./GitInstall.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOADINF="git.inf"
 
 
