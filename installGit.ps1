65b467aaee3a317f9441da3e545fbe576f14e23b
gvrx <62734313+GVRX@users.noreply.github.com>
2025-02-18 14:50:19 +1100

    fixed git path problem

--- installGit.ps1
+++ installGit.ps1
@@ -1,18 +1,23 @@
 # Install Git in current directory 
 
-Write-Host "`n Installing GIT..." -ForegroundColor Yellow
 
 $currentDirectory = Get-Location
 $mqitPath = "$currentDirectory\mqit"
+$git_path = "$mqitPath\Git"
+$gitBinPath = "$gitPath\bin"
+
 
 # Add git CLI utilities to path
 $PATH = [Environment]::GetEnvironmentVariable("PATH")
-$git_path = "$($mqitPath)\Git"
-[Environment]::SetEnvironmentVariable("PATH", "$PATH;$git_path\bin")
+[Environment]::SetEnvironmentVariable("PATH", "$PATH;$gitBinPath")
 
+# append git path to git.inf 
 Add-Content -Path "git.inf" -Value "Dir=$($git_path)"
 
+
+# get and install Git
+Write-Host "`n Downloading GIT installer ..." -ForegroundColor Yellow
 curl -o "./GitInstall.exe" "https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe"
+
+Write-Host "`n Installing GIT..." -ForegroundColor Yellow
 ./GitInstall.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOADINF="git.inf"
-
-
